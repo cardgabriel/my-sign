dc31817d90147617d7b3d51b6e323791b7b3d00b
cardgabriel <cardgabriel98@gmail.com>
2021-07-04 12:47:53 -0300

    Revert "add auth with firebase"
    This reverts commit a60b08203a0ff606d41135899c5ccf9f183bac53.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthProvider } from "./auth/auth";
 import Home from "./pages/Home";
 import HoroscopoFlow from "./pages/HoroscopoFlow";
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/stepper">
-            <HoroscopoFlow />
-          </Route>
-        </Switch>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/stepper">
+          <HoroscopoFlow />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 

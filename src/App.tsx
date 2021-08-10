0edb3bd4bade33d8e10ed4529d80b25fac7a3b5c
cardgabriel <cardgabriel98@gmail.com>
2021-08-10 00:52:18 -0300

    changes project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HoroscopoFlow from "./components/HoroscopoFlow";
+import Navbar from "components/Navbar";
+import StepperSign from "components/StepperSign";
+import DatosStep from "components/steps/DatosStep";
+import HoroscopoStep from "components/steps/HoroscopoStep";
+import React, { useEffect, useState } from "react";
 
+enum STEPS {
+  datos,
+  horoscopo,
+}
 const App: React.FC = () => {
+  const [currentStep, setCurrentStep] = useState(STEPS.datos);
+  const [labelAppBar, setLabelAppBar] = useState<string>();
+  const [labelStepper, setLabelStepper] = useState<string>();
+
+  useEffect(() => {
+    if (currentStep === STEPS.datos) {
+      setLabelAppBar("TUS DATOS");
+      setLabelStepper("Ingresa tus datos");
+    } else if (currentStep === STEPS.horoscopo) {
+      setLabelAppBar("TU HORÓSCOPO");
+      setLabelStepper("");
+    }
+  }, [currentStep]);
+
+  const stepsLength = Object.keys(STEPS).length / 2;
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HoroscopoFlow />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      {/* NAVBAR */}
+      <Navbar
+        currentStep={currentStep}
+        onBackClick={() => {
+          setCurrentStep(currentStep - 1);
+        }}
+        label={labelAppBar}
+      />
+
+      {/* STEPPER */}
+      <StepperSign
+        activeStep={currentStep}
+        length={stepsLength}
+        label={labelStepper}
+      />
+
+      {/* STEPS */}
+
+      {currentStep === STEPS.datos && (
+        <DatosStep
+          onBack={() => {
+            setCurrentStep(currentStep - 1);
+          }}
+          onNext={() => setCurrentStep(STEPS.horoscopo)}
+        />
+      )}
+
+      {currentStep === STEPS.horoscopo && <HoroscopoStep />}
+    </>
   );
 };
 
